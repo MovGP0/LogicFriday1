9de08360626579a81e2d27c54213ee7605f5b452
Johann Dirry <movgp0@gmail.com>
2026-05-23 17:49:21 +0200

    Implement gate palette placement buttons

--- LogicFriday1/Controls/GateDiagramSurface.cs
+++ LogicFriday1/Controls/GateDiagramSurface.cs
@@ -31,8 +31,9 @@ public sealed class GateDiagramSurface : Control
     {
         base.OnPointerPressed(e);
 
-        if (SelectedPaletteItem is not { Kind: GatePaletteKind.Nand, InputCount: 2 } item ||
+        if (SelectedPaletteItem is not { } item ||
             Items is null ||
+            !IsPlaceable(item) ||
             !e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
         {
             return;
@@ -86,49 +87,241 @@ public sealed class GateDiagramSurface : Control
 
     private static void DrawItem(DrawingContext context, GateDiagramItem item)
     {
-        if (item.Kind == GatePaletteKind.Nand && item.InputCount == 2)
+        var pen = new Pen(Brushes.Black, 1.5);
+        var textBrush = Brushes.Black;
+        var origin = new Point(item.X, item.Y);
+
+        Point P(double x, double y)
         {
-            DrawNand2(context, item);
+            return new Point(origin.X + x, origin.Y + y);
+        }
+
+        switch (item.Kind)
+        {
+            case GatePaletteKind.Not:
+                DrawNot(context, P, pen);
+                break;
+
+            case GatePaletteKind.Nand:
+                DrawAnd(context, true, item.InputCount, P, pen);
+                break;
+
+            case GatePaletteKind.And:
+                DrawAnd(context, false, item.InputCount, P, pen);
+                break;
+
+            case GatePaletteKind.Nor:
+                DrawOr(context, true, false, item.InputCount, P, pen);
+                break;
+
+            case GatePaletteKind.Or:
+                DrawOr(context, false, false, item.InputCount, P, pen);
+                break;
+
+            case GatePaletteKind.Xor:
+                DrawOr(context, false, true, item.InputCount, P, pen);
+                break;
+
+            case GatePaletteKind.Mux:
+                DrawMux(context, P, pen, textBrush);
+                break;
+
+            case GatePaletteKind.ConstantZero:
+                DrawConstant(context, "0", P, pen, textBrush);
+                break;
+
+            case GatePaletteKind.ConstantOne:
+                DrawConstant(context, "1", P, pen, textBrush);
+                break;
         }
     }
 
-    private static void DrawNand2(DrawingContext context, GateDiagramItem item)
+    private static void DrawNot(DrawingContext context, Func<double, double, Point> p, Pen pen)
     {
-        var pen = new Pen(Brushes.Black, 1.5);
-        var textBrush = Brushes.Black;
-        var x = item.X;
-        var y = item.Y;
-
         var geometry = new StreamGeometry();
         using (var stream = geometry.Open())
         {
-            stream.BeginFigure(new Point(x + 55, y), false);
-            stream.LineTo(new Point(x + 15, y));
-            stream.LineTo(new Point(x + 15, y + 50));
-            stream.LineTo(new Point(x + 55, y + 50));
-            stream.ArcTo(
-                new Point(x + 55, y),
-                new Size(25, 25),
-                0,
-                false,
-                SweepDirection.CounterClockwise);
+            stream.BeginFigure(p(15, 5), false);
+            stream.LineTo(p(15, 45));
+            stream.LineTo(p(55, 25));
+            stream.EndFigure(true);
+        }
+
+        context.DrawGeometry(null, pen, geometry);
+        context.DrawEllipse(null, pen, p(59.5, 25), 4.5, 4.5);
+        context.DrawLine(pen, p(0, 25), p(15, 25));
+        context.DrawLine(pen, p(64, 25), p(100, 25));
+    }
+
+    private static void DrawAnd(
+        DrawingContext context,
+        bool inverted,
+        int inputCount,
+        Func<double, double, Point> p,
+        Pen pen)
+    {
+        var geometry = new StreamGeometry();
+        using (var stream = geometry.Open())
+        {
+            stream.BeginFigure(p(55, 0), false);
+            stream.LineTo(p(15, 0));
+            stream.LineTo(p(15, 50));
+            stream.LineTo(p(55, 50));
+            stream.CubicBezierTo(p(80, 50), p(80, 0), p(55, 0));
             stream.EndFigure(false);
         }
 
         context.DrawGeometry(null, pen, geometry);
-        context.DrawEllipse(null, pen, new Point(x + 83.5, y + 25), 4.5, 4.5);
-        context.DrawLine(pen, new Point(x + 88, y + 25), new Point(x + 100, y + 25));
-        context.DrawLine(pen, new Point(x, y + 10), new Point(x + 15, y + 10));
-        context.DrawLine(pen, new Point(x, y + 40), new Point(x + 15, y + 40));
+
+        if (inverted)
+        {
+            context.DrawEllipse(null, pen, p(83.5, 25), 4.5, 4.5);
+            context.DrawLine(pen, p(88, 25), p(100, 25));
+        }
+        else
+        {
+            context.DrawLine(pen, p(80, 25), p(100, 25));
+        }
+
+        DrawInputLines(context, inputCount, p, pen, 15, []);
+    }
+
+    private static void DrawOr(
+        DrawingContext context,
+        bool inverted,
+        bool xor,
+        int inputCount,
+        Func<double, double, Point> p,
+        Pen pen)
+    {
+        var inputOffset = xor ? -7 : 0;
+        var inputAdjustments = GetOrInputAdjustments(inputCount);
+
+        var geometry = new StreamGeometry();
+        using (var stream = geometry.Open())
+        {
+            stream.BeginFigure(p(35, 0), false);
+            stream.LineTo(p(15, 0));
+            stream.CubicBezierTo(p(25, 12), p(25, 38), p(15, 50));
+            stream.LineTo(p(35, 50));
+            stream.CubicBezierTo(p(50, 50), p(75, 37), p(80, 25));
+            stream.CubicBezierTo(p(75, 13), p(55, 0), p(35, 0));
+            stream.EndFigure(false);
+        }
+
+        context.DrawGeometry(null, pen, geometry);
+
+        if (xor)
+        {
+            var xorGeometry = new StreamGeometry();
+            using (var stream = xorGeometry.Open())
+            {
+                stream.BeginFigure(p(8, 0), false);
+                stream.CubicBezierTo(p(18, 12), p(18, 38), p(8, 50));
+                stream.EndFigure(false);
+            }
+
+            context.DrawGeometry(null, pen, xorGeometry);
+        }
+
+        if (inverted)
+        {
+            context.DrawEllipse(null, pen, p(84.5, 25), 4.5, 4.5);
+            context.DrawLine(pen, p(89, 25), p(100, 25));
+        }
+        else
+        {
+            context.DrawLine(pen, p(80, 25), p(100, 25));
+        }
+
+        DrawInputLines(context, inputCount, p, pen, 15 + inputOffset, inputAdjustments);
+    }
+
+    private static void DrawMux(DrawingContext context, Func<double, double, Point> p, Pen pen, IBrush textBrush)
+    {
+        context.DrawRectangle(null, pen, new Rect(p(15, 0), p(65, 50)));
+        context.DrawLine(pen, p(0, 10), p(15, 10));
+        context.DrawLine(pen, p(0, 25), p(15, 25));
+        context.DrawLine(pen, p(0, 40), p(15, 40));
+        context.DrawLine(pen, p(65, 25), p(100, 25));
+        DrawText(context, "D0", p(18, 3), textBrush, 9);
+        DrawText(context, "D1", p(18, 18), textBrush, 9);
+        DrawText(context, "S", p(18, 33), textBrush, 9);
+        DrawText(context, "OUT", p(40, 18), textBrush, 8);
+    }
+
+    private static void DrawConstant(
+        DrawingContext context,
+        string value,
+        Func<double, double, Point> p,
+        Pen pen,
+        IBrush textBrush)
+    {
+        context.DrawLine(pen, p(55, 25), p(40, 25));
+        context.DrawLine(pen, p(40, 20), p(40, 30));
+        DrawText(context, value, p(22, 14), textBrush, 15);
+    }
+
+    private static void DrawInputLines(
+        DrawingContext context,
+        int inputCount,
+        Func<double, double, Point> p,
+        Pen pen,
+        double targetX,
+        IReadOnlyList<double> targetAdjustments)
+    {
+        var offset = inputCount switch
+        {
+            2 => 30,
+            3 => 15,
+            4 => 10,
+            _ => 30
+        };
+
+        for (var index = 0; index < inputCount; index++)
+        {
+            var y = 10 + index * offset;
+            var adjustment = index < targetAdjustments.Count ? targetAdjustments[index] : 0;
+            context.DrawLine(pen, p(0, y), p(targetX + adjustment, y));
+        }
+    }
+
+    private static void DrawText(DrawingContext context, string text, Point point, IBrush brush, double fontSize)
+    {
         context.DrawText(
             new FormattedText(
-                item.Label,
+                text,
                 CultureInfo.CurrentCulture,
                 FlowDirection.LeftToRight,
                 Typeface.Default,
-                11,
-                textBrush),
-            new Point(x + 22, y + 55));
+                fontSize,
+                brush),
+            point);
+    }
+
+    private static IReadOnlyList<double> GetOrInputAdjustments(int inputCount)
+    {
+        return inputCount switch
+        {
+            2 => [4, 4],
+            3 => [0, 0, 4],
+            4 => [7, 4, 0, 4],
+            _ => []
+        };
+    }
+
+    private static bool IsPlaceable(GatePaletteItem item)
+    {
+        return item.Kind is
+            GatePaletteKind.Not or
+            GatePaletteKind.Nand or
+            GatePaletteKind.Nor or
+            GatePaletteKind.Mux or
+            GatePaletteKind.And or
+            GatePaletteKind.Or or
+            GatePaletteKind.Xor or
+            GatePaletteKind.ConstantZero or
+            GatePaletteKind.ConstantOne;
     }
 
     private static double Snap(double value)
